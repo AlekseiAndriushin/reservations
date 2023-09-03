f6945661fd2095c0067220acdf88d2c1b9c2009d
Alexey Andryushin <andr200116alex@gmail.com>
2023-09-03 20:19:27 +0300

    feat: add db

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,39 @@
 package api
 
 import (
+	"context"
+
+	"github.com/AlexeyAndryushin/reservations/db"
 	"github.com/AlexeyAndryushin/reservations/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+	id = c.Params("id")
+	ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	user := types.User{
 		FirstName: "Aleksus",
 		LastName: "Alekseev",
 	}
 	return c.JSON(user)
-}
-
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("Alekseus")
 }
--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/AlexeyAndryushin/reservations/api"
+	"github.com/AlexeyAndryushin/reservations/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+var config = fiber.Config{
+    ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return c.JSON(map[string]string{"error": err.Error()})
+    },
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":3000", "The listend address of the API server")
 	flag.Parse()
-  app := fiber.New()
+
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//hanlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+  app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
   app.Listen(*listenAddr)
 }
